e12c18ba07c4894b864a1456e7a01d9231b4d033
Nelson Drozd <nelsondrozd@gmail.com>
2022-12-10 21:26:11 -0500

    Worked on models, still need Ingredient subdocument.

--- server/models/Bag.js
+++ server/models/Bag.js
@@ -1,20 +1,31 @@
 const { Schema, Types } = require('mongoose');
+const userSchema = require('./User')
 
 const bagSchema = new Schema(
     {
         bagId: {
+            // ID logic taken from Mongo mini-project, unsure if this is correct, and this goes for all ID's
             type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId
+            default: () => new Types.ObjectId,
+            required: true
         },
         gameId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId
-        }, // Unsure about gameId?
+        }, 
         userId: {
-
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
         },
         bagIngredients: [
-            
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Ingredient',
+        }
         ]
     }
 )
+
+const Bag = model('bag', bagSchema);
+
+module.exports = Bag;
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,13 @@
-const { Schema, model, Types} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const gameSchema = require('./Game');
+const bagSchema = require('./Bag');
+const listSchema = require('./List')
 
 const userSchema = new Schema(
     {
         userId: {
-
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
         },
         userName: {
             type: String,
@@ -11,8 +15,9 @@ const userSchema = new Schema(
         },
         userEmail: {
             type: String,
-            required: true
-            // validators here?
+            required: true,
+            match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email']
+            // Regex found here: https://gist.github.com/rupeshtiwari/acf770bfc85f3fe1f62a80b461abfc13
         },
         userPW: {
             type: String,
@@ -20,10 +25,27 @@ const userSchema = new Schema(
             // hashing logic
         },
         userGames: [
-
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Game',
+            }
         ],
         userBags: [
-            
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Bag',
+            }
+        ],
+        userLists: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+
         ]
     }
 )
+
+const User = model('user', userSchema);
+
+module.exports = User;
