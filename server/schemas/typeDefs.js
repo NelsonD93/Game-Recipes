0d8f7799a05304aea2c670001e8335ee9d216677
Alex Bellamy <alexjbellamy@gmail.com>
2022-12-12 19:01:34 -0500

    added stub files for typedefs / resolvers. added server auth code

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+
+
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
