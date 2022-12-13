599eee53d5fd3cb6e9946ecba315a0262d1329e8
Alex Bellamy <alexjbellamy@gmail.com>
2022-12-12 20:14:41 -0500

    added beginnings of build list

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const {  } = require('../models');
+const { Bag, Game, Item, List, User  } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
 
@@ -9,6 +9,11 @@ const resolvers = {
     },
     Mutation: {
 
+        // Mutation to build a shopping list of raw materials needed to build an item. Takes an itemId as an argument and returns a list of ingredients
+        buildList: async (parent, { itemId }) => {
+            const buildItem = await Item.findOne({_id: itemId});
+            const listArray = [];
+        }
     }
 };
 
