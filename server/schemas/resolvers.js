401911c8db943cbc4e470d30b4ac57dff85fff20
alextheordinary <alexjbellamy@gmail.com>
2022-12-12 21:11:37 -0500

    Update resolvers.js
    Looks good! Made a few changes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,22 +11,22 @@ const resolvers = {
         return Game.find({})
     },
     getOneItem: async(parent, {itemId} ) => {
-    return await Item.findOne({_id: itemId})
+    return Item.findOne({_id: itemId})
     },
     getGameItems: async(gameId) => {
-        return await Item.find({_id: gameId})
+        return Item.find({_id: gameId})
     },
-    getBag: async (gameId) => {
-        return await Bag.find({_id: gameId})
+    getBag: async (gameId, userId) => {
+        return Bag.find({gameId: gameId, userId: userId})
     },
     getOneUser: async (userId) => {
-        return await User.findOne({_id: userId})
+        return User.findOne({_id: userId})
     },
     getGameUsers: async (gameId) => {
-        return await User.find({})
+        return User.find({}) // Maybe just scrap / ignore for now
     },
     getList: async (listId) => {
-        return await List.find({_id: listId})
+        return List.find({_id: listId})
     }
     },
     Mutation: {
