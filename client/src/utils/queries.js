2f8d04ac03df921e21a130f4fb61bdd48cc4e74f
KerriAnn1 <dragonsfire20@hotmail.com>
2022-12-15 19:00:42 -0500

    query updates

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -70,3 +70,35 @@ export const GET_ONE_USER = gql`
     }
 `;
 
+export const GET_LIST= gql`
+    query getList ($userId:ID!) {
+    user (listId: $listId) {
+        _id
+        name
+        userId
+        ingredients
+        completed
+        buildStack
+    }
+    }
+`;
+
+export const GET_USERS = gql`
+    query getUsers {
+    getUsers {
+        _id
+        name
+        }
+    }
+`;
+
+export const GET_LISTS = gql`
+    query getLists {
+    getLists {
+        _id
+        name
+        }
+    }
+`;
+
+
