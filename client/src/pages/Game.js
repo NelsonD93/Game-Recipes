f2b886f8b7d9a5701a9d884f9dea9c2549506b49
Nelson Drozd <nelsondrozd@gmail.com>
2022-12-16 19:48:20 -0500

    Fixed class errors on Game.js page

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import 'w3-css/w3.css';
-import '../assets/css/Game.css';
 import mineLogo from '../../src/assets/images/minecraft.png';
 import botwLogo from '../../src/assets/images/botw.png';
 import mineBG from '../../src/assets/images/minecraftBG.jpg';
-import ListComponent from '../components/ListComponent';
 
 import Navbar from '../components/Navbar';
 
@@ -15,31 +13,31 @@ function Game() {
         <div>
             <Navbar />
             <div className="w3-display-container w3-content w3-wide">
-                <img src={mineBG} alt='minecraft background'/>
+                <img src={mineBG} alt='Minecraft background'/>
                 <div className="w3-display-middle w3-margin-top w3-center">
                     <h1 className="w3-xxlarge w3-text-white">
-                        <span className="w3-padding w3-black w3-opacity-min alex-test">
+                        <span className="w3-padding w3-black w3-opacity-min ">
                             <b>WIKICRAFT</b></span>
                         <span className="w3-hide-small w3-text-light-grey"></span>
                     </h1>
                 </div>
             </div>
 
-            <div class="w3-container w3-padding-32" id="games">
-                <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Games</h3>
+            <div className="w3-container w3-padding-32" id="games">
+                <h3 className="w3-border-bottom w3-border-light-grey w3-padding-16">Games</h3>
             </div>
 
-            <div class="w3-row-padding">
-                <div class="w3-col l3 m6 w3-margin-bottom">
-                    <div class="w3-display-container">
-                        <div class="w3-display-topleft w3-black w3-padding">Minecraft</div>
+            <div className="w3-row-padding">
+                <div className="w3-col l3 m6 w3-margin-bottom">
+                    <div className="w3-display-container">
+                        <div className="w3-display-topleft w3-black w3-padding">Minecraft</div>
                         <img src={mineLogo} className="mineblock" alt="minecraft grass block" />
                     </div>
                 </div>
 
-                <div class="w3-col l3 m6 w3-margin-bottom">
-                    <div class="w3-display-container">
-                        <div class="w3-display-topleft w3-black w3-padding">Breath of the Wild</div>
+                <div className="w3-col l3 m6 w3-margin-bottom">
+                    <div className="w3-display-container">
+                        <div className="w3-display-topleft w3-black w3-padding">Breath of the Wild</div>
 
                         <img src={botwLogo} className="zeldaLogo" alt="zelda breath of the wild logo"/>
 
@@ -47,7 +45,7 @@ function Game() {
                 </div>
 
             </div>
-            <ListComponent/>
+
         </div>
     );
 }
