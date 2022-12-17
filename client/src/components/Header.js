cc4dd42c72a5b6f4a41a106b81fef27debbb9d4c
Graham Johnson <107967591+graham1996@users.noreply.github.com>
2022-12-17 12:00:08 -0500

    moved header image to home, fixed sizing issues

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import 'w3-css/w3.css';
-// import image from '../../src/images/botwBG.jpg';
+// import image from '../../src/assets/images/botwBG.jpg';
+import '../assets/css/Game.css';
 
 function Header() {
 
     return (
 
         <div>
-            <div className="w3-display-container w3-content w3-wide" style={{
-                backgroundImage: `url('/images/botwBG.jpg')`,
-                backgroundRepeat: 'no-repeat',
-                height: '100vh',
-                width: '100%',
-            }}>
+            <div className="w3-display-container w3-content w3-wide">
+                {/* <div>
+                    <img src={image} class="homeBG"/>
+                </div> */}
                 <div className="w3-display-middle w3-margin-top w3-center">
                     <h1 className="w3-xxlarge w3-text-white">
                         <span className="w3-padding w3-black w3-opacity-min ">
