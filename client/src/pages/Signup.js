eb6638ef9b0b765f2371ba17f088f84a5365779f
LizRanZim <lrzclass22@gmail.com>
2022-12-18 13:22:10 -0500

    fix login and signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -42,7 +42,7 @@ const Signup = () => {
   
 
   return (
-    <div className="w3-padding-48">
+    <div className="w3-padding-48 main">
       <div className="w3-display-container">
         <div className="w3-display-topleft w3-black w3-padding">Signup</div>
         <div className="w3-padding-48">
