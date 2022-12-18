6a9aba4fc7eddb45151e2b75c917afab11f3d566
Alex Bellamy <alexjbellamy@gmail.com>
2022-12-18 13:19:28 -0500

    basic gather page

---
+++ client/src/pages/Gather.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import 'w3-css/w3.css';
+import '../assets/css/Game.css';
+// import mineLogo from '../../src/assets/images/minecraft.png';
+// import botwLogo from '../../src/assets/images/botw.png';
+// import mineBG from '../../src/assets/images/minecraftBG.jpg';
+// import GameRecipeComponent from '../components/GameRecipesComponent';
+import ListComponent from '../components/ListComponent';
+import { useLocation } from 'react-router-dom';
+
+
+
+
+function Game() {
+    const location = useLocation();
+    const { listId } = location.state;
+
+
+    return (
+        <div>
+            <ListComponent listId={listId}/>
+        </div>
+    );
+}
+
+export default Game;
